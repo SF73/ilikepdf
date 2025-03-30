dad738140ba08f5b0e37d8164cff904b061f1ab5
Sylvain Finot <25269226+SF73@users.noreply.github.com>
2025-03-30 16:39:28 +0200

    Change python tree

--- python/ilikepdfpy/setup.py
+++ python/ilikepdfpy/setup.py
@@ -2,9 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ilikepdfpy",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=["PyMuPDF"],
-    description='A toolbox based on PyMuPDF for PDF manipulation',
-    author='Sylvain Finot',
 )
