9ccce081016bf347075bfa5e75d69058a16bf824
Sylvain Finot <25269226+SF73@users.noreply.github.com>
2025-04-12 16:17:50 +0200

    Tiny clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,14 @@
-// import { useEffect, useState } from "react";
-//import FileExplorer from './components/FileExplorer';
-import ExtractImages from './components/ExtractImages';
 import { HashRouter, Routes, Route, Outlet } from "react-router";
-// import Home from "./components/Home";
-// import MetadataEditor from "./components/MetadataEditor";
-// import Split from "./components/Split";
-// import Merge from "./components/Merge";
 import NavBar from "./components/NavBar";
-// import Thumbnail from "./components/Thumbnail";
-// import ExtractPTEX from "./components/ExtractPTEX";
-// import Test from './components/Test';
-// import ArrangePagesString from './components/ArrangePagesString';
+
 
 
 import MetadataWorker from './components/MetadataWorker';
 import ExtractImagesWorker from './components/ExtractImagesWorker';
 import SelectPagesWorker from './components/SelectPagesWorker';
-// Layout component that always displays Navbar and CustomComponent
+import HomeWorker from './components/HomeWorker';
+
+
 const Layout = () => (
   <div className="flex flex-col sm:flex-row min-h-screen">
     <NavBar />
@@ -32,15 +24,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home />} /> */}
-          {/* <Route path="extract-images" element={<ExtractImages />} />
-          <Route path="metadata" element={<MetadataEditor />} />
-          <Route path="ptex" element={<ExtractPTEX />} />
-          <Route path="split" element={<Split />} />
-          <Route path="merge" element={<Merge />} />
-          <Route path="thumbnail" element={<Thumbnail />} />
-          <Route path="test" element={<Test />} />
-          <Route path="arrange-pages" element={<ArrangePagesString />} /> */}
+          <Route index element={<HomeWorker />} />
           <Route path="metadata-worker" element={<MetadataWorker />} />
           <Route path="extract-images-worker" element={<ExtractImagesWorker />} />
           <Route path="select-pages-worker" element={<SelectPagesWorker />} />
